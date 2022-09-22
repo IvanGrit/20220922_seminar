19aed5f9a5347bd964d6ff766b26700daabd6090
Ivan Gr <of_2000@mail.ru>
2022-09-22 22:57:31 +0400

    006

---
+++ 006_Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Кораблики
+
+using static System.Console;
+Clear();
+Write("Введите сделанных корабликов: ");
+int Number = Convert.ToInt32(ReadLine());
+double Ship = Number/6;
+WriteLine($"Девочка сделала {Ship*4} корабликов");
+WriteLine($"Каждый мальчик сделал по {Ship} корабликов");
