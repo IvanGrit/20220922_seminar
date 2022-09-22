cdb5bf543de9b79390fcf4a12971b20cb13ab32f
Ivan Gr <of_2000@mail.ru>
2022-09-22 22:57:18 +0400

    005

---
+++ 005_Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using static System.Console;
+Clear();
+Write("Введите выбитых банок первым стрелком: ");
+int Number1 = Convert.ToInt32(ReadLine());
+Write("Введите выбитых банок вторым стрелком: ");
+int Number2 = Convert.ToInt32(ReadLine());
+WriteLine($"Первый стрелок не выбил {Number2-1} банок");
+WriteLine($"Второй стрелок не выбил {Number1-1} банок");
